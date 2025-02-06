3b7abeb88325a0059a7b1d119fcd73af899a03b9
DLikelyUni <duncan.likely.2021@uni.strath.ac.uk>
2025-02-06 23:10:26 +0000

    Working 5G signal modulated to 3GHz

--- SigGen_script.m
+++ SigGen_script.m
@@ -3,7 +3,7 @@
 
 %%
 
-NCOfreq = 4.725;  % RF DAC NCO frequency (GHz)
+NCOfreq = 3;  % RF DAC NCO frequency (GHz)
 NCOphase = 0;  % RF DAC NCO phase 
-FsDAC = 2048; % DAC output sample rate (MSPS)
+FsDAC = 7000; % DAC output sample rate (MSPS)
 ImagOutput = zeros(1,100);
